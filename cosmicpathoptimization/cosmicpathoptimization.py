5f4a7d524b1858c7bfce25352fe642a24e2a21f0
NateBarnaik <nbarnaiks@gmail.com>
2023-10-24 19:54:40 +0000

    updated cosmicpathoptimization.py

--- cosmicpathoptimization/cosmicpathoptimization.py
+++ cosmicpathoptimization/cosmicpathoptimization.py
@@ -4,4 +4,21 @@ from typing import List
 
 
 def answer(temps: List[int]) -> int:
-    return 0
+    """Finds and prints the mean from a list of integers, as a integer
+
+    Args:
+        temps (List[int]): List of temps
+
+    Returns:
+        int: Mean of the list of temps, printed as an int
+    """
+    return int(statistics.mean(temps))
+
+
+def main() -> None:
+    """Main function
+    """
+
+
+if __name__ == "__main__":
+    main()
